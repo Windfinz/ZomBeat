ed6dc908921af1a807e0e7be75ad642a46feb09d
Windfinz <Phongdraco@gmail.com>
2023-11-13 14:53:25 +0700

    health bar

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image healthBar;
+
+    public float health;
+    public float maxHealth;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            
+        }
+
+    }
+
+    private void Update()
+    {
+        HealthBar();
+    }
+
+    private void HealthBar()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+
+}
